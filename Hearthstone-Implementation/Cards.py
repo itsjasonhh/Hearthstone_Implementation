0dba011e25a4f18ed7c3b655e4854fdd06e97cf9
jason <jahhuang@ucsc.edu>
2020-07-08 12:08:41 -0700

    Hearthstone implementation started

---
+++ Hearthstone-Implementation/Cards.py
@@ -0,0 +1,18 @@
+class Creature:
+    def __init__(self, name, cost, attack, health, tag, battlecry, effect):
+        self.name = name
+        self.cost = cost
+        self.attack = attack
+        self.current_health = health
+        self.total_health = health
+        self.tag = tag
+        self.battlecry = battlecry
+        self.effect = effect
+
+
+class Spell:
+    def __init__(self,name,cost):
+        self.name = name
+        self.cost = cost
+
+
